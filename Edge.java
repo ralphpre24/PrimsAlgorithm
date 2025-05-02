0c87a2a4eb7e5e78e29f702fa66374ea92917ec4
Ralph Nikko Predas <142536413+ralphpre24@users.noreply.github.com>
2025-05-03 04:06:58 +0800

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -10,3 +10,13 @@
             this.weight = weight;
         }
     }
+
+ public void addEdge(int source, int destination, int weight) {
+            Edge edge = new Edge(source, destination, weight);
+            adjacencyList[source].addFirst(edge);
+            
+            // Since graph is undirected, add reverse edge also
+            edge = new Edge(destination, source, weight);
+            adjacencyList[destination].addFirst(edge);
+        }
+    }
