8223a7f00633bf0c8665a3c851b0598157ec2837
Ralph Nikko Predas <142536413+ralphpre24@users.noreply.github.com>
2025-05-03 04:05:43 +0800

    Create Graph.java

---
+++ Graph.java
@@ -0,0 +1,13 @@
+  // Class to represent a graph
+   public class Graph {
+        int vertices;
+        LinkedList<Edge>[] adjacencyList;
+        
+        Graph(int vertices) {
+            this.vertices = vertices;
+            adjacencyList = new LinkedList[vertices];
+            
+            for (int i = 0; i < vertices; i++) {
+                adjacencyList[i] = new LinkedList<>();
+            }
+        }
